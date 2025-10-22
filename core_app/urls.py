cd3c7c47ab00d38f2a1a3d557cc9ace6724d1e0f
mairanaz <151368125+mairanaz@users.noreply.github.com>
2025-10-22 14:34:55 +0800

    Revert "Initial commit - RBI Automation Django Project"
    This reverts commit 53283524c3d2b86f3a10852ad2ecba475842862d.

--- core_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.upload_drawing, name="upload_drawing"),
-    path("equipments/", views.equipment_list, name="equipment_list"),
-]
